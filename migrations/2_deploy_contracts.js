17d0bb4cdf1a274a9785f888d09c0ce357d9ca83
futo <futo@disarmista.com>
2019-01-31 17:18:43 -0300

    We are now using chained promises instead of async/await for deployment, resulting in a more robust process.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,20 +5,24 @@ const Merkle = artifacts.require('./Merkle');
 const Challenge = artifacts.require('./Challenge');
 
 module.exports = function(deployer) {
-	async function doDeploy(deployer) {
-		deployer.deploy(StandardToken, "Token", "TOK", 2, 1000000);
-		let i = await StandardToken.deployed();
 
-		deployer.deploy(Merkle);
-		deployer.deploy(Challenge);
-		deployer.link(Challenge, BatPay);
-		deployer.link(Merkle, BatPay);
-		deployer.deploy(BatPay, i.address);
-		await BatPay.deployed();
-		
-		deployer.link(Merkle, TestHelper);
-		deployer.deploy(TestHelper);
-		await TestHelper.deployed();
-	}
-	doDeploy(deployer);
+	let token;
+
+	deployer.deploy(StandardToken, "Token", "TOK", 2, 1000000)
+	.then(x=>StandardToken.deployed()).then(i=>{
+			token = i;
+			return deployer.deploy(Merkle);
+		})
+	.then(x=>deployer.deploy(Challenge)).then(()=>{
+			deployer.link(Challenge, BatPay);
+			deployer.link(Merkle, BatPay);	
+			deployer.link(Merkle,TestHelper);
+			return Merkle.deployed();
+		})
+	.then(x=>deployer.deploy(TestHelper))
+	.then(x=>Challenge.deployed())
+	.then(x=>deployer.deploy(BatPay, token.address));
+	
+
+
 };
