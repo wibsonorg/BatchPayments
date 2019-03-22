46e2a6d830af2aa7b8abda6d059a67ccf565ccc4
Nico <ncls.ayala@gmail.com>
2019-03-22 10:38:05 -0300

    Add getDataSum tests (still failing)

--- test/test_challenge_pure_fns.js
+++ test/test_challenge_pure_fns.js
@@ -1,5 +1,9 @@
+const truffleAssertions = require('truffle-assertions');
 const Challenge = artifacts.require('Challenge');
+const { getChallengeData } = require('../lib/bat');
 
+const { reverts, ErrorType } = truffleAssertions;
+const assertRevert = (promise, message) => reverts(promise, ErrorType.REVERT, message);
 let challenge;
 
 contract.only('Challenge', () => {
@@ -9,13 +13,31 @@ contract.only('Challenge', () => {
 
     describe('getDataSum', () => {
         it('returns the sum of the amounts in data', async () => {
-            const data = '0x000000000000000a00000005000000000000000a00000006000000000000000a00000007000000000000000a00000008';
+            const data = getChallengeData([10, 15, 20, 25], [0, 100, 1000, 10000]);
             const result = await challenge.getDataSum(data);
-            assert.equal(Number(result), 40, "wrong summarization");
+            assert.equal(Number(result), 70, 'wrong summarization');
         });
-        it('returns the sum of the amounts even when there is only one element in data');
-        it('rejects when the data has wrong format');
-        it('rejects when the summarization causes an overflow');
+        it('returns the sum of the amounts even when there is only one element in data', async () => {
+            const data = getChallengeData([10], [0]);
+            const result = await challenge.getDataSum(data);
+            assert.equal(Number(result), 10, 'wrong summarization');
+        });
+        it('rejects when the data has wrong format', async () => {
+            await assertRevert(challenge.getDataSum('0x'), 'must fail when data is zero length');
+            await assertRevert(
+                challenge.getDataSum('0x0000000a0005'),
+                'must fail when record size is lower than expected',
+            );
+            await assertRevert(
+                challenge.getDataSum('0x0000000000000000000a000000000005'),
+                'must fail when record is greater than expected',
+            );
+        });
+        it('rejects when the summarization causes an overflow', async () => {
+            await assertRevert(
+                challenge.getDataSum('0xffffffffffffffff00000005000000000000000100000005'),
+            );
+        });
     });
 
     describe('getDataAtIndex', () => {
