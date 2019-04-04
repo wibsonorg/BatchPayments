45e594df618ec178981b7baaa1f88df10878a336
Agustin Modugno <agustin.modugno@grandata.com>
2019-04-04 11:13:03 -0300

    Using SafeMath assert/require to validate

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -98,12 +98,7 @@ contract StandardToken is IERC20 {
     }
 
     function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-        uint oldValue = allowed[msg.sender][_spender];
-        if (_subtractedValue > oldValue) {
-            allowed[msg.sender][_spender] = 0;
-        } else {
-            allowed[msg.sender][_spender] = SafeMath.sub(oldValue, _subtractedValue);
-        }
+        allowed[msg.sender][_spender] = SafeMath.sub(allowed[msg.sender][_spender], _subtractedValue);
         emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
