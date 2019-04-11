95b9f6ab2d57d27efb58e426e2c8256d68c2d26c
@azavalla <azavalla@dc.uba.ar>
2019-04-11 18:42:09 -0300

    Refactor coverage provider into lib

--- truffle.js
+++ truffle.js
@@ -1,47 +1,4 @@
-const solcVersion = '0.4.25'
-
-// To prevent truffle from complaining 'provider' isn't defined when compiling
-const ProviderEngine = require('web3-provider-engine')
-const provider = new ProviderEngine()
-
-const { GanacheSubprovider } = require('@0x/subproviders')
-
-const { CoverageSubprovider } = require('@0x/sol-coverage')
-const { TruffleArtifactAdapter } = require('@0x/sol-trace')
-
-const projectRoot = ''
-const defaultFromAddress = '0x5409ed021d9299bf6814279a6a1411a7e866a631'
-const partialConfig = {
-  ignoreFilesGlobs: [
-    '**/contracts/TestHelper.sol',
-    '**/contracts/MassExitLib.sol'
-  ],
-  isVerbose: true
-}
-const artifactAdapter = new TruffleArtifactAdapter(projectRoot, solcVersion)
-
-global.coverageSubprovider = new CoverageSubprovider(
-  artifactAdapter,
-  defaultFromAddress,
-  partialConfig
-)
-
-provider.addProvider(global.coverageSubprovider)
-
-const ganacheSubprovider = new GanacheSubprovider({
-  'default_balance_ether': '1000000000000000000000000',
-  'total_accounts': 10,
-  'port': 8545,
-})
-
-provider.addProvider(ganacheSubprovider)
-
-provider.start(err => {
-  if (err !== undefined) {
-    console.log(err)
-    process.exit(1)
-  }
-})
+const coverageWeb3Provider = require('./lib/CoverageWeb3Provider')
 
 module.exports = {
   networks: {
@@ -51,7 +8,7 @@ module.exports = {
       network_id: '*' // Match any network id
     },
     coverage: {
-      provider,
+      provider: coverageWeb3Provider,
       network_id: '*'
     }
   },
