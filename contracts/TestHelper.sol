412e6a4ed9ce685e448af5f4397132516729a5e1
Nico <ncls.ayala@gmail.com>
2019-04-12 12:45:00 -0300

    Remove the need for TestHelper.skip

--- contracts/TestHelper.sol
+++ contracts/TestHelper.sol
@@ -9,8 +9,6 @@ import "./SafeMath.sol";
  */
 contract TestHelper {
 
-    function skip() public {}
-
     /**
      * @dev wrapper. see `Merkle` contract
      */
--- test/test_batpay.js
+++ test/test_batpay.js
@@ -14,13 +14,6 @@ const merkle = lib.merkle
 var test
 var unlockBlocks
 
-async function skipBlocks (n) {
-  let v = []
-  for (let i = 0; i < n; i++) { v.push(test.skip()) }
-
-  for (let i = 0; i < n; i++) { await v[i] }
-}
-
 contract('BatPay', (addr) => {
   let a0 = addr[0]
   let a1 = addr[1]
