0f58139a7f0d2f22c655adce5ecc4fadf52514ce
Nico <ncls.ayala@gmail.com>
2019-04-12 10:19:30 -0300

    Fix separate-by-one-line-in-contract linter issue

--- contracts/TestHelper.sol
+++ contracts/TestHelper.sol
@@ -13,7 +13,6 @@ contract TestHelper {
     /// @dev Calculate the hash of given bytes
     /// @param x a bytes value
     /// @return the hash value
-
     function hash(bytes x) public pure returns(bytes32) {
         return keccak256(abi.encodePacked(x));
     }
@@ -21,7 +20,6 @@ contract TestHelper {
     /// @dev Recover signer address from a message by using their signature
     /// @param _hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
     /// @param _sig bytes signature, the signature is generated using `web3.eth.sign()`
-
     function recover(bytes32 _hash, bytes _sig)
         public
         pure
@@ -62,7 +60,6 @@ contract TestHelper {
 
     /// @dev prefix a bytes32 value with "\x19Ethereum Signed Message:"
     ///      and hash the result
-
     function toEthSignedMessageHash(bytes32 _hash)
         public
         pure
@@ -82,7 +79,6 @@ contract TestHelper {
     /// @param fromPay TBC
     /// @param toPay TBC
     /// @param amount TBC
-
     function getHashForCollect(
         uint32 delegate,
         uint32 toId,
@@ -103,7 +99,6 @@ contract TestHelper {
     /// @param v TBC
     /// @param r TBC
     /// @param s TBC
-
     function recoverHelper(
         bytes32 hashv,
         uint8 v,
@@ -118,20 +113,17 @@ contract TestHelper {
     }
 
     /// @dev wrapper. see `Merkle` contract
-
     function combinedHash(bytes32 a, bytes32 b) public pure returns (bytes32) {
         return Merkle.combinedHash(a, b);
     }
 
     /// @dev wrapper. see `Merkle` contract
-
     function getProofRootHash(bytes32[] memory proof, uint256 key, bytes32 value) public pure returns (bytes32) {
         return Merkle.getProofRootHash(proof, key, value);
     }
 
     /// TODO: complete this one
     /// @dev TBC
-
     function test(uint y, bytes memory x) public returns (uint256) {
         uint256 ret = y;
         for (uint i = 0; i < x.length && i < 32; i++) ret = ret * 256 + uint(x[i]);
@@ -141,7 +133,6 @@ contract TestHelper {
 
     /// TODO: complete this one
     /// @dev TBC
-
     function testlen(uint y, bytes memory x) public returns (uint256) {
         if (y == 1234) return 0;
         return x.length;
@@ -149,79 +140,64 @@ contract TestHelper {
 
     // SafeMath functions
     // uint64
-
     /// @dev wrapper. see `SafeMath` contract
-
     function mul64(uint256 a, uint256 b) public returns (uint64) {
         return SafeMath.mul64(a, b);
     }
 
     /// @dev wrapper. see `SafeMath` contract
-
     function div64(uint256 a, uint256 b) public returns (uint64) {
         return SafeMath.div64(a, b);
     }
 
     /// @dev wrapper. see `SafeMath` contract
-
     function sub64(uint256 a, uint256 b) public returns (uint64) {
         return SafeMath.sub64(a, b);
     }
 
     /// @dev wrapper. see `SafeMath` contract
-
     function add64(uint256 a, uint256 b) public returns (uint64) {
         return SafeMath.add64(a, b);
     }
 
     // uint32
-
     /// @dev wrapper. see `SafeMath` contract
-
     function mul32(uint256 a, uint256 b) public returns (uint32) {
         return SafeMath.mul32(a, b);
     }
 
     /// @dev wrapper. see `SafeMath` contract
-
     function div32(uint256 a, uint256 b) public returns (uint32) {
         return SafeMath.div32(a, b);
     }
 
     /// @dev wrapper. see `SafeMath` contract
-
     function sub32(uint256 a, uint256 b) public returns (uint32) {
         return SafeMath.sub32(a, b);
     }
 
     /// @dev wrapper. see `SafeMath` contract
-
     function add32(uint256 a, uint256 b) public returns (uint32) {
         return SafeMath.add32(a, b);
     }
 
     // uint256
-
     /// @dev wrapper. see `SafeMath` contract
-
     function mul(uint256 a, uint256 b) public returns (uint256) {
         return SafeMath.mul(a, b);
     }
 
     /// @dev wrapper. see `SafeMath` contract
-
     function div(uint256 a, uint256 b) public returns (uint256) {
         return SafeMath.div(a, b);
     }
 
     /// @dev wrapper. see `SafeMath` contract
-
     function sub(uint256 a, uint256 b) public returns (uint256) {
         return SafeMath.sub(a, b);
     }
 
     /// @dev wrapper. see `SafeMath` contract
-
     function add(uint256 a, uint256 b) public returns (uint256) {
         return SafeMath.add(a, b);
     }
